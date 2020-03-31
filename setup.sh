03ec5a131360bce277dbd7966964e85c7e642a03
sadfsdf <mamogaaa@yandex.ru>
2020-03-31 11:32:07 +0300

    fixes

--- setup.sh
+++ setup.sh
@@ -5,28 +5,28 @@ fi
 echo "Your ip is $EXTERNAL_IP"
 SCRIPT_DIR="$(cd $(dirname $0) && pwd)"
 echo "Script dir: $SCRIPT_DIR"
-# echo "Installing docker..."
-# apt-get update
-# apt-get install -y \
-#     apt-transport-https \
-#     ca-certificates \
-#     curl \
-#     gnupg-agent \
-#     software-properties-common
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-# apt-key fingerprint 0EBFCD88
-# add-apt-repository \
-#    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-#    $(lsb_release -cs) \
-#    stable"
-# apt-get update
-# apt-get install -y docker-ce docker-ce-cli containerd.io
-# ln -s "$SCRIPT_DIR/restart.sh" /usr/bin/restart_openvpn
-# ln -s "$SCRIPT_DIR/start.sh" /usr/bin/start_openvpn
-# ln -s "$SCRIPT_DIR/stop.sh" /usr/bin/stop_openvpn
-# ln -s "$SCRIPT_DIR/create_openvpn_user.sh" /usr/bin/create_openvpn_user
+echo "Installing docker..."
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+ln -s "$SCRIPT_DIR/restart.sh" /usr/bin/restart_openvpn
+ln -s "$SCRIPT_DIR/start.sh" /usr/bin/start_openvpn
+ln -s "$SCRIPT_DIR/stop.sh" /usr/bin/stop_openvpn
+ln -s "$SCRIPT_DIR/create_openvpn_user.sh" /usr/bin/create_openvpn_user
 
-# docker run -v /root/ovpn-data:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_genconfig -u "tcp://$EXTERNAL_IP:443"
-# docker run -v /root/ovpn-data:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn ovpn_initpki
+docker run -v /root/ovpn-data:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_genconfig -u "tcp://$EXTERNAL_IP:443"
+docker run -v /root/ovpn-data:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn ovpn_initpki
 
-# docker run --name openvpn --restart always -v /root/ovpn-data:/etc/openvpn -d -p 443:1194 --cap-add=NET_ADMIN kylemanna/openvpn
+docker run --name openvpn --restart always -v /root/ovpn-data:/etc/openvpn -d -p 443:1194 --cap-add=NET_ADMIN kylemanna/openvpn
