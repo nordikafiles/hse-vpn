16de4740aa580bdaaebda3e2bbb0da9825a142d9
sadfsdf <mamogaaa@yandex.ru>
2020-06-17 21:00:35 +0300

    fixes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -z "$EXTERNAL_IP" ]; then
-    EXTERNAL_IP=$(curl -s https://api.ipify.org)
+  EXTERNAL_IP=$(curl -s https://api.ipify.org)
 fi
 read -a input -p "Enter external ip [$EXTERNAL_IP]: "
 EXTERNAL_IP=${input:-$EXTERNAL_IP}
@@ -10,22 +10,28 @@ read -a input -p "Enter server port [$PORT]: "
 PORT=${input:-$PORT}
 
 SCRIPT_DIR="$(cd $(dirname $0) && pwd)"
-echo "Installing docker..."
-apt-get update
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+
+if [ -x "$(command -v docker)" ]; then
+  echo "Installed: $(docker -v)"
+else
+  echo "Installing docker..."
+  apt-get update
+  apt-get install -y \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      gnupg-agent \
+      software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+  apt-key fingerprint 0EBFCD88
+  add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+  apt-get update
+  apt-get install -y docker-ce docker-ce-cli containerd.io
+fi
+
 ln -s "$SCRIPT_DIR/restart.sh" /usr/bin/restart_openvpn
 ln -s "$SCRIPT_DIR/start.sh" /usr/bin/start_openvpn
 ln -s "$SCRIPT_DIR/stop.sh" /usr/bin/stop_openvpn
